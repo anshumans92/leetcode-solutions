0549971496e0e63ff1dd8bde3df8561fa9d5aadb
anshumans92 <43419550+anshumans92@users.noreply.github.com>
2019-08-20 19:09:37 -0700

    added comments

--- Remove Vowels from String.py
+++ Remove Vowels from String.py
@@ -1,9 +1,11 @@
 class Solution:
     def removeVowels(self, S: str) -> str:
+        #check if input string is present
         if not str:
             return
         ret = ""
         vowels = ['a','e','i','o','u']
+        #basic for loop check
         for i in S:
             if i not in vowels:
                 ret+=i
